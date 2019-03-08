d8c81b0229a570a8d0cb3e6d4739c563c7674b25
Cameron <cameronjmaccabe@gmail.com>
2019-03-08 00:46:28 -0800

    added GIF generation functionality via the GIPHY API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-let topics = ["spongebob", "patrick", "squidward", "mr krabs", "sandy", "plankton", "mrs puff", "larry", "bikini bottom", "band geeks"]
+let topics = ["SpongeBob", "Patrick", "Squidward", "Mr Krabs", "Plankton", "Sandy", "Mrs Puff", "Larry", "Band Geeks", "campfire"];
 
 function generateButtons() {
 
@@ -7,23 +7,56 @@ function generateButtons() {
     for (i = 0; i < topics.length; i++) {
       let button = $("<button>");
       button.addClass("spongey-topic");
-      button.attr("data-name", topics[i]);
+      button.attr("spongey-data", topics[i]);
       button.text(topics[i]);
       $("#buttons").append(button);
     }
 
 }
 
-generateButtons();
-
-$("#add-topic").on("click", function(event) {
+function addTopic() {
 
     event.preventDefault();
-
     let topic = $("#spongey-input").val().trim();
-
     topics.push(topic);
-
     generateButtons();
 
-});
+};
+
+function addSponginess() {
+
+    let sponginess = $(this).attr("spongey-data");
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sponginess.toLowerCase() + "-spongebob-squarepants&api_key=" + config.apiKey + "&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        let results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+
+        let spongeyDiv = $("<div>");
+        let title = $("<p>");
+        $(title).html("<br><br>" + results[i].title.toUpperCase());
+        let p = $("<p>");
+        $(p).text("Rating: " + results[i].rating.toUpperCase());
+        let spongeyImage = $("<img>");
+        spongeyImage.attr("src", results[i].images.fixed_height.url);
+        spongeyDiv.append(title);
+        spongeyDiv.append(p);
+        spongeyDiv.append(spongeyImage);
+        $("#gifs").prepend(spongeyDiv);
+
+        }
+
+    });
+
+};
+
+generateButtons();
+
+$(document).on("click", "#add-topic", addTopic);
+
+$(document).on("click", ".spongey-topic", addSponginess);
